4f6373ae259e71f1f3bbea5e19ed9132e8a33e02
tchesa <cesarolimpio@gmail.com>
2023-03-17 16:12:53 -0300

    cube rotation

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,17 @@
 import {
   BufferGeometry,
   DirectionalLight,
+  Euler,
   Line,
   LineBasicMaterial,
+  Mesh,
+  MeshBasicMaterial,
   Object3D,
   OrthographicCamera,
   Raycaster,
   Scene,
+  Shape,
+  ShapeGeometry,
   Vector2,
   Vector3,
   WebGLRenderer,
@@ -44,6 +49,7 @@ const camera = new OrthographicCamera(
   0.3,
   1000
 );
+// camera.rotateZ(Math.PI / 4);
 camera.position.z = 5;
 
 // const textureLoader = new TextureLoader();
@@ -68,78 +74,85 @@ window.scene.add(directionalLight);
 // box.scale.set(4, 4, 4);
 const cube = new Cube({
   FRONT: [
+    [{ division: "LEFT_RIGHT" }, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined],
+    // [
+    //   { division: "LEFT_RIGHT" },
+    //   { division: "LEFT_RIGHT" },
+    //   // { division: "LEFT_RIGHT" },
+    //   undefined,
+    //   { division: "BOT_LEFT_TOP", isEmitter: true, isStatic: true },
+    // ],
+    [undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined],
+  ],
+  RIGHT: [
+    [undefined, { division: "LEFT_RIGHT" }, undefined, undefined],
+    [undefined, undefined, undefined, undefined],
+    // [
+    //   { division: "LEFT_RIGHT" },
+    //   { division: "LEFT_RIGHT" },
+    //   { division: "LEFT_RIGHT" },
+    //   { division: "BOT_LEFT_TOP", isEmitter: true },
+    // ],
+    [undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined],
+  ],
+  BACK: [
+    [undefined, undefined, { division: "LEFT_RIGHT" }, undefined],
+    [undefined, undefined, undefined, undefined],
+    // [
+    //   { division: "LEFT_RIGHT" },
+    //   { division: "LEFT_RIGHT" },
+    //   { division: "LEFT_RIGHT" },
+    //   { division: "BOT_LEFT_TOP", isEmitter: true },
+    // ],
+    [undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined],
+  ],
+  LEFT: [
     [undefined, undefined, undefined, { division: "LEFT_RIGHT" }],
     [undefined, undefined, undefined, undefined],
     [
       { division: "LEFT_RIGHT" },
       { division: "LEFT_RIGHT" },
-      // { division: "LEFT_RIGHT" },
-      undefined,
-      { division: "BOT_LEFT_TOP", isEmitter: true, isStatic: true },
+      { division: "LEFT_RIGHT" },
+      { division: "BOT_LEFT_TOP", isEmitter: true },
     ],
     [undefined, undefined, undefined, undefined],
   ],
-  // RIGHT: [
-  //   [undefined, undefined, undefined, undefined],
-  //   [undefined, undefined, undefined, undefined],
-  //   [
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "BOT_LEFT_TOP", isEmitter: true },
-  //   ],
-  //   [undefined, undefined, undefined, undefined],
-  // ],
-  // BACK: [
-  //   [undefined, undefined, undefined, undefined],
-  //   [undefined, undefined, undefined, undefined],
-  //   [
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "BOT_LEFT_TOP", isEmitter: true },
-  //   ],
-  //   [undefined, undefined, undefined, undefined],
-  // ],
-  // LEFT: [
-  //   [undefined, undefined, undefined, undefined],
-  //   [undefined, undefined, undefined, undefined],
-  //   [
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "BOT_LEFT_TOP", isEmitter: true },
-  //   ],
-  //   [undefined, undefined, undefined, undefined],
-  // ],
-  // TOP: [
-  //   [undefined, undefined, undefined, undefined],
-  //   [undefined, undefined, undefined, undefined],
-  //   [
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "BOT_LEFT_TOP", isEmitter: true },
-  //   ],
-  //   [undefined, undefined, undefined, undefined],
-  // ],
-  // BOT: [
-  //   [undefined, undefined, undefined, undefined],
-  //   [undefined, undefined, undefined, undefined],
-  //   [
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "LEFT_RIGHT" },
-  //     { division: "BOT_LEFT_TOP", isEmitter: true },
-  //   ],
-  //   [undefined, undefined, undefined, undefined],
-  // ],
+  TOP: [
+    [undefined, undefined, undefined, undefined],
+    [{ division: "LEFT_RIGHT" }, undefined, undefined, undefined],
+    [
+      { division: "LEFT_RIGHT" },
+      { division: "LEFT_RIGHT" },
+      { division: "LEFT_RIGHT" },
+      { division: "BOT_LEFT_TOP", isEmitter: true },
+    ],
+    [undefined, undefined, undefined, undefined],
+  ],
+  BOT: [
+    [undefined, undefined, undefined, undefined],
+    [undefined, { division: "LEFT_RIGHT" }, undefined, undefined],
+    [
+      { division: "LEFT_RIGHT" },
+      { division: "LEFT_RIGHT" },
+      { division: "LEFT_RIGHT" },
+      { division: "BOT_LEFT_TOP", isEmitter: true },
+    ],
+    [undefined, undefined, undefined, undefined],
+  ],
 });
 
 const mousePosition = new Vector2();
 let dragging: Object3D | undefined;
 let draggingInitialPosition = new Vector3();
 let pointerOffset = new Vector3();
+let rotating = false;
+let verticalRotation = false;
+let rotationInitialPoint = new Vector2();
+let cubeInitialRotation = new Euler();
 
 window.addEventListener("mousemove", function (e) {
   mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
@@ -184,6 +197,30 @@ window.addEventListener("mousemove", function (e) {
         0
       );
     }
+  } else if (rotating) {
+    const offset = new Vector2(
+      mousePosition.x - rotationInitialPoint.x,
+      mousePosition.y - rotationInitialPoint.y
+    );
+    const rotationScale = 2;
+
+    cube.mesh.rotation.set(
+      cubeInitialRotation.x,
+      cubeInitialRotation.y,
+      cubeInitialRotation.z,
+      cubeInitialRotation.order
+    );
+    if (verticalRotation) {
+      cube.mesh.rotateOnWorldAxis(
+        new Vector3(1, 0, 0),
+        -offset.y * rotationScale
+      );
+    } else {
+      cube.mesh.rotateOnWorldAxis(
+        new Vector3(0, 1, 0),
+        offset.x * rotationScale
+      );
+    }
   }
 });
 
@@ -200,6 +237,12 @@ window.addEventListener("mousedown", function (e) {
   );
   const blockIntersection = intersects.find(
     (intersect) => intersect.object.name === BLOCK_MESH_NAME
+  );
+
+  const triangleIntersection = intersects.find(
+    (intersect) =>
+      intersect.object.name === Triangle.VERTICAL ||
+      intersect.object.name === Triangle.HORIZONTAL
   );
 
   if (
@@ -234,6 +277,12 @@ window.addEventListener("mousedown", function (e) {
       pointerLocalPosition.y - dragging.position.y,
       0
     );
+  } else if (triangleIntersection) {
+    const hit = triangleIntersection.point;
+    rotating = true;
+    verticalRotation = triangleIntersection.object.name === Triangle.VERTICAL;
+    rotationInitialPoint.set(mousePosition.x, mousePosition.y);
+    cubeInitialRotation = cube.mesh.rotation.clone();
   }
 });
 
@@ -242,9 +291,17 @@ window.addEventListener("mouseup", function (e) {
     Block.map[dragging.uuid].toggleSelected(false);
     Block.map[dragging!.uuid].snapPosition();
     cube.checkEletrified();
+  } else if (rotating) {
+    cube.mesh.rotation.set(
+      (Math.round((cube.mesh.rotation.x / Math.PI) * 2) / 2) * Math.PI,
+      (Math.round((cube.mesh.rotation.y / Math.PI) * 2) / 2) * Math.PI,
+      (Math.round((cube.mesh.rotation.z / Math.PI) * 2) / 2) * Math.PI,
+      cubeInitialRotation.order
+    );
   }
 
   dragging = undefined;
+  rotating = false;
 });
 
 const raycaster = new Raycaster();
@@ -255,27 +312,80 @@ const raycaster = new Raycaster();
 const cubeMargin = 0.05;
 const bufferGeometry = new BufferGeometry();
 const geometry = bufferGeometry.setFromPoints([
-  new Vector3(-GRID_SIZE / 2 - cubeMargin, -GRID_SIZE / 2 - cubeMargin, 0),
-  new Vector3(GRID_SIZE / 2 + cubeMargin, -GRID_SIZE / 2 - cubeMargin, 0),
-  new Vector3(GRID_SIZE / 2 + cubeMargin, GRID_SIZE / 2 + cubeMargin, 0),
-  new Vector3(-GRID_SIZE / 2 - cubeMargin, GRID_SIZE / 2 + cubeMargin, 0),
-  new Vector3(-GRID_SIZE / 2 - cubeMargin, -GRID_SIZE / 2 - cubeMargin, 0),
+  new Vector3(-GRID_SIZE / 2 - cubeMargin, -GRID_SIZE / 2 - cubeMargin, 2),
+  new Vector3(GRID_SIZE / 2 + cubeMargin, -GRID_SIZE / 2 - cubeMargin, 2),
+  new Vector3(GRID_SIZE / 2 + cubeMargin, GRID_SIZE / 2 + cubeMargin, 2),
+  new Vector3(-GRID_SIZE / 2 - cubeMargin, GRID_SIZE / 2 + cubeMargin, 2),
+  new Vector3(-GRID_SIZE / 2 - cubeMargin, -GRID_SIZE / 2 - cubeMargin, 2),
 ]);
 window.scene.add(new Line(geometry, new LineBasicMaterial({ color: "gray" })));
+
+const triangleScale = 5;
+
+enum Triangle {
+  HORIZONTAL = "TRIANGLE_HORIZONTAL",
+  VERTICAL = "TRIANGLE_VERTICAL",
+}
+
+const rightTriangleShape = new Shape();
+rightTriangleShape.moveTo(0, 0);
+rightTriangleShape.lineTo(2 * triangleScale, -2 * triangleScale);
+rightTriangleShape.lineTo(2 * triangleScale, 2 * triangleScale);
+
+const rightTriangleGeometry = new ShapeGeometry(rightTriangleShape);
+const triangleMaterial = new MeshBasicMaterial({
+  color: "pink",
+  wireframe: true,
+  transparent: true,
+  opacity: 0,
+});
+const rightTriangle = new Mesh(rightTriangleGeometry, triangleMaterial);
+rightTriangle.name = Triangle.HORIZONTAL;
+window.scene.add(rightTriangle);
+
+const leftTriangleShape = new Shape();
+leftTriangleShape.moveTo(0, 0);
+leftTriangleShape.lineTo(-2 * triangleScale, -2 * triangleScale);
+leftTriangleShape.lineTo(-2 * triangleScale, 2 * triangleScale);
+
+const leftTriangleGeometry = new ShapeGeometry(leftTriangleShape);
+const leftTriangle = new Mesh(leftTriangleGeometry, triangleMaterial);
+leftTriangle.name = Triangle.HORIZONTAL;
+window.scene.add(leftTriangle);
+
+const topTriangleShape = new Shape();
+topTriangleShape.moveTo(0, 0);
+topTriangleShape.lineTo(2 * triangleScale, 2 * triangleScale);
+topTriangleShape.lineTo(-2 * triangleScale, 2 * triangleScale);
+
+const topTriangleGeometry = new ShapeGeometry(topTriangleShape);
+const topTriangle = new Mesh(topTriangleGeometry, triangleMaterial);
+topTriangle.name = Triangle.VERTICAL;
+window.scene.add(topTriangle);
+
+const bottomTriangleShape = new Shape();
+bottomTriangleShape.moveTo(0, 0);
+bottomTriangleShape.lineTo(2 * triangleScale, -2 * triangleScale);
+bottomTriangleShape.lineTo(-2 * triangleScale, -2 * triangleScale);
+
+const bottomTriangleGeometry = new ShapeGeometry(bottomTriangleShape);
+const bottomTriangle = new Mesh(bottomTriangleGeometry, triangleMaterial);
+bottomTriangle.name = Triangle.VERTICAL;
+window.scene.add(bottomTriangle);
 
 function animate(time: number) {
   renderer.render(window.scene, camera);
 }
 renderer.setAnimationLoop(animate);
 
-const gui = new dat.GUI();
-const options = {
-  cubeX: 0,
-  cubeY: 0,
-};
-gui.add(options, "cubeX", 0, 2).onChange(function (e) {
-  cube.mesh.rotation.x = e * Math.PI;
-});
-gui.add(options, "cubeY", 0, 2).onChange(function (e) {
-  cube.mesh.rotation.y = e * Math.PI;
-});
+// const gui = new dat.GUI();
+// const options = {
+//   cubeX: 0,
+//   cubeY: 0,
+// };
+// gui.add(options, "cubeX", 0, 2).onChange(function (e) {
+//   cube.mesh.rotation.x = e * Math.PI;
+// });
+// gui.add(options, "cubeY", 0, 2).onChange(function (e) {
+//   cube.mesh.rotation.y = e * Math.PI;
+// });
