d45304cf68bd8efbb97bbdc052305db4e3abcc94
tchesa <cesarolimpio@gmail.com>
2023-03-19 19:54:15 -0300

    remove console.log

--- src/js/index.ts
+++ src/js/index.ts
@@ -89,7 +89,6 @@ window.scene.add(directionalLight);
 const cube = new Cube(stage);
 cube.shuffleAll();
 cube.checkEletrified();
-console.log(cube.grids);
 // const cube = new Cube({
 //   FRONT: [
 //     [
